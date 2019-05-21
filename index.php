bf1b8193e9d15433eca4c4ede751107f662cb307
codeandclay <oliver@codeandclay.com>
2019-05-21 17:16:04 +0100

    Extract article to partial

--- index.php
+++ index.php
@@ -6,32 +6,8 @@
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
-			?>
-	<article>
-		<h1><?php the_title(); ?></h1>
-		<div class="post_date">
-			<a href="<?php echo( esc_html( get_permalink() ) ); ?>"> <?php the_date(); ?></a>
-		</div>
-			<?php
-				the_content();
-			?>
-			<a href="<?php echo( esc_html( get_permalink() ) ); ?>">
-				comments(<?php echo( esc_html( get_comments_number() ) ); ?>)
-			</a>
-		<div class="categories">
-			<ul>
-			<?php
-				$categories = get_the_category();
-			foreach ( $categories as $category ) {
-				$category_id = $category->cat_ID;
-				echo( '<a href="' . esc_attr( get_category_link( $category_id ) ) . '"><li>' . esc_html( $category->name ) . '</li></a>' );
-			}
-			?>
-			</ul>
-		</div>
-	</article>
-			<?php
-		endwhile;
+			get_template_part( 'partials/article' );
+			endwhile;
 		else :
 			esc_html_e( 'Sorry. There are no posts.', 'useful-plugins' );
 		endif;
