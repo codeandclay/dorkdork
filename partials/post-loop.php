69a56404165b7cc93e98e79b658ecc0a263d14dc
codeandclay <oliver@codeandclay.com>
2019-05-27 23:09:22 +0100

    Simplify next post navigation
    Only display link to next post.
    
    Because I want to.

--- partials/post-loop.php
+++ partials/post-loop.php
@@ -4,14 +4,10 @@ if ( have_posts() ) {
     the_post();
     get_template_part( 'partials/article' );
   }
-  ?>
-  <div class="pagination">Next:
-    <?php
-    previous_post_link('%link', '%title');
-    ?>
-  </div>
-  <?php
-  get_template_part('partials/comments');
+  $previous_post = get_previous_post_link('%link', '%title');
+  if ( ! empty( $previous_post) ) {
+    echo('<div class="pagination">Next:&nbsp;' . $previous_post . '</div>');
+  }
 } else {
   esc_html_e( 'Sorry. There are no posts.', 'useful-plugins' );
 }
